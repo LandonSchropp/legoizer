c5d7a9cd0501ebaf61abdc2c4415b4adfb5dcc62
Landon Schropp <schroppl@gmail.com>
2017-03-19 00:56:40 -0700

    Fix bug in Color#to_s

--- color.rb
+++ color.rb
@@ -15,7 +15,7 @@ class Color
   end
 
   def to_s
-    "##{ red.to_s(16) }#{ green.to_s(16) }#{ blue.to_s(16) }"
+    "##{ to_a.map { |n| n.to_s(16).rjust(2, "0") }.join }"
   end
 
   alias_method :inspect, :to_s
