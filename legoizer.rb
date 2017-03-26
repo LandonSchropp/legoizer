1c87c57bacfc37c8b119cf09337f7318e28b7035
Landon Schropp <schroppl@gmail.com>
2017-03-25 21:36:49 -0700

    Refactor image logic into separate class

--- legoizer.rb
+++ legoizer.rb
@@ -3,6 +3,7 @@ require "mini_magick"
 
 require_relative "color"
 require_relative "lego_blueprint"
+require_relative "image"
 
 def exit_with_error
   puts "Usage: ruby legoizer.rb <path-to-image> <width-in-millimeters> <draw-outlines>"
@@ -30,35 +31,20 @@ image_path = ARGV[0]
 image_width = ARGV[1].to_f
 draw_outlines = ARGV[2] == "true"
 
-# Read in the image
-image = MiniMagick::Image.open(image_path)
+# Read the image
+image = Image.read(image_path)
 
 # Determine the number of Legos to use
-image_brick_width = (image_width / BRICK_MILLIMETER_WIDTH).round
-image_brick_height = (image_width / image.width * image.height / BRICK_MILLIMETER_HEIGHT).round
+blueprint_width = (image_width / BRICK_MILLIMETER_WIDTH).round
+blueprint_height = (image_width / image.width * image.height / BRICK_MILLIMETER_HEIGHT).round
 
-# Resize the image to the correct number of pixels and read it into a color array
-image.scale "#{ image_brick_width }x#{ image_brick_height }!"
-
-# Use ChunkyPNG to read the image pixel by pixel
-image.format "png"
-chunky_image = ChunkyPNG::Image.from_io(StringIO.new(image.to_blob))
-
-# Convert the image to a 2D array of colors
-lego_colors = (0...chunky_image.width).map do |x|
-  (0...chunky_image.height).map do |y|
-
-    red = (chunky_image[x, y] & 0xff000000) >> 24
-    green = (chunky_image[x, y] & 0x00ff0000) >> 16
-    blue = (chunky_image[x, y] & 0x0000ff00) >> 8
-    alpha = (chunky_image[x, y] & 0x000000ff)
-
-    Color.new(red: red, green: green, blue: blue, alpha: alpha)
-  end
-end
+# Convert the image to a 2D array of colors by resizing the image so each pixel represents a block
+# in the blueprint.
+image.scale(blueprint_width, blueprint_height)
+colors = image.to_a
 
 # Output the blueprint.
 LegoBlueprint
-  .new(image_brick_width, image_brick_height, lego_colors)
+  .new(blueprint_width, blueprint_height, colors)
   .to_chunky_png(outline: draw_outlines)
   .save('lego.png', interlace: true)
